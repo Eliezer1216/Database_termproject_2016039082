ed0f16c75dc7441590d3996daf9c4a1552078285
(shinhyo) <(tlsgyals1216@gmail.com)>
2022-12-04 23:38:54 +0900

    Login Function 70%

---
+++ src/InBodyFunctions/Login.java
@@ -0,0 +1,55 @@
+package InBodyFunctions;
+
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Login {
+    //public int Studentnum;
+    //public int password;
+    public int backmode;
+    Scanner sc=new Scanner((System.in));
+
+    public int Login()
+    {
+        int Studentnum;
+        int Password;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.56.102:4567/software_InBody ", "root",
+                    "7632"); // software_InBody 데이터베이스에 접근
+            Statement stmt = (Statement) con.createStatement();
+            System.out.println("\n*******************************************");
+            System.out.println("로그인 화면");
+            System.out.println("*******************************************");
+            System.out.println("\n학번을 입력하세요:");
+            Studentnum=sc.nextInt();
+            System.out.println("비밀번호를 입력하세요:");
+            Password=sc.nextInt();
+            ResultSet rs=stmt.executeQuery("SELECT 학번,비밀번호 FROM 회원 WHERE 학번="+Studentnum);
+            rs.next();
+            if(Password==rs.getInt(2))
+            {
+                System.out.println("\n로그인에 성공하셨습니다!!");
+            }
+            else {
+                System.out.println("\n로그인에 실패하셨습니다!!");
+            }
+            //while(rs.next())
+            //System.out.println(rs.getInt(1)+" "+rs.getInt(2));
+            con.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+
+        return 0;
+    }
+    // 회원테이블에 정보 비교 함수 리턴 값
+    // 로그인에 실패하면 계속 실패했다고 알려주기
+    // 뒤로가기는 3
+
+}
+
---
+++ src/InBodyFunctions/Main.java
@@ -0,0 +1,70 @@
+package InBodyFunctions;
+
+import InBodyFunctions.Login;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        int mode;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://192.168.56.102:4567/software_InBody ", "root",
+                    "7632"); // software_InBody 데이터베이스에 접근
+            Statement stmt = (Statement) con.createStatement();
+            do {
+                mode=FirstScreen();
+                if((mode!=1)&&(mode!=2)&&(mode!=3))
+                    System.out.println("\n다시 입력하세요!!");
+            }while((mode!=1)&&(mode!=2)&&(mode!=3));
+            if((mode==1))
+            {
+                Login login=new Login();
+                login.Login();
+
+            }
+            else if((mode==2))
+            {
+                System.out.println("2번 기능");
+            }
+            else if((mode==3))
+            {
+                System.out.println("3번 기능");
+            }
+            else
+            {
+
+
+
+            }
+            // ResultSet rs=stmt.executeQuery("SELECT * FROM 회원");
+            // while(rs.next())
+            //System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4)+" "+rs.getInt(5));
+            //con.close();
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+
+
+
+
+    }
+    public static int FirstScreen()
+    {
+
+        int F_mode;
+        Scanner sc=new Scanner((System.in));
+        System.out.println("\n-----------------------------------------------------------");
+        System.out.println("Welcome to Software Inbody database~~~!!!!!");
+        System.out.println("1. 로그인 2. 회원가입 3. 프로그램 종료");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("\n원하는 기능의 숫자를 입력하세요 :");
+        F_mode=sc.nextInt();
+        return F_mode;
+    }
+
+}
